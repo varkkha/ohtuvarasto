c072068608a9073840878a73ef71d2bd24c43d5f
Hanna Varkki <h.varkki@gmail.com>
2024-11-01 13:35:29 +0200

    unittesting

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,46 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+        
+    def test_lisays_negatiivinen_maara(self):
+        self.varasto.lisaa_varastoon(-5)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+        
+    def test_lisays_yli_tilavuuden(self):
+        self.varasto.lisaa_varastoon(15)  
+        self.assertAlmostEqual(self.varasto.saldo, self.varasto.tilavuus)  
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 0)
+        
+    def test_ottaminen_negatiivinen_maara(self):
+        self.varasto.lisaa_varastoon(5)
+        saatu_maara = self.varasto.ota_varastosta(-3)  
+        self.assertAlmostEqual(saatu_maara, 0)  
+        
+    def test_ottaminen_yli_saldon(self):
+        self.varasto.lisaa_varastoon(5)
+        saatu_maara = self.varasto.ota_varastosta(10)  
+        self.assertAlmostEqual(saatu_maara, 5)
+        self.assertAlmostEqual(self.varasto.saldo, 0) 
+    
+    def test_konstruktori_nollatilavuus(self):
+        varasto = Varasto(0)
+        self.assertAlmostEqual(varasto.tilavuus, 0.0)  
+        self.assertAlmostEqual(varasto.saldo, 0.0)    
+
+    def test_konstruktori_negatiivinen_tilavuus(self):
+        varasto = Varasto(-5)
+        self.assertAlmostEqual(varasto.tilavuus, 0.0)  
+        self.assertAlmostEqual(varasto.saldo, 0.0)     
+
+    def test_konstruktori_saldo_yhtaa_tilavuuden(self):
+        varasto = Varasto(10, 15)
+        self.assertAlmostEqual(varasto.saldo, varasto.tilavuus) 
+        
+    def test_konstruktori_negatiivinen_saldo(self):
+        varasto = Varasto(10, -5)
+        self.assertAlmostEqual(varasto.saldo, 0.0)
+        
+    def test_str_metodi(self):
+        self.varasto.lisaa_varastoon(5)
+        expected_str = f"saldo = 5.0, vielä tilaa 5.0"
+        self.assertEqual(str(self.varasto), expected_str) 
